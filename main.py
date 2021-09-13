e22ade26111bc8cb62b2b24ea6376122c43e52e9
jkmpod <80140745+jkmpod@users.noreply.github.com>
2021-09-13 10:58:09 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Do not edit the function's name!
+
+def add(a, b):
+    """ a function to add two integers"""
+    # Remove the '#' in the subsequent lines and replace with your code
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    return a+b
+
+# Do not edit code below this
+a, b = map(int, input().split())
+print(add(a, b))
+
+# If you are having a doubt or is facing challenges in the coding please make the variable doubt = 1 in the below function 
+def have_doubt():
+  doubt = 0
+  return doubt
